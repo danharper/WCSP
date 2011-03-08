fbf57c8ecec8b5ceec7affc43221020e13f26c42
Dan Harper <intouch@danharper.me>
2011-03-08 11:46:20 +0000

    Sidebar basket

--- functional/partials/sidebar.php
+++ functional/partials/sidebar.php
@@ -2,7 +2,7 @@
 
 <aside>
 	<section id="cart">
-		<p>You have <span class="items">4</span> items in your shopping cart, totalling <span class="price">$32</span>.</p>
+		<p>You have <span class="items"><?php echo sizeof($_SESSION['basket']) ?></span> items in your shopping cart, totalling <span class="price">$32</span>.</p>
 		<p><a href="<?php echo $this->link_to('basket'); ?>">View Basket</a></p>
 	</section>
 	<form id="search">
