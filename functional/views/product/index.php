1a2dd4492c118be38af7a3b6d2d303a7eefd612b
Dan Harper <intouch@danharper.me>
2011-03-23 20:58:33 +0000

    LIs

--- functional/views/product/index.php
+++ functional/views/product/index.php
@@ -1,11 +1,13 @@
 <ol>
 <?php
 foreach ($products as $p) {
+	echo '<li>';
 	$image = $images[$p->id];
 	echo '<h2><a href="'. $this->link_to('product', 'show', $p->id) .'">'. $p->title .'</a></h2>';
 	echo '<p>'. nl2br($p->description) .'</p>';
 	echo '<p><em>£'. $p->price .'</em></p>';
 	echo '<img src="'.ROOT.'/static/productimages/'.$p->id.'/'. $image->name .'" alt="'. $image->alt .'" height="150">';
 	echo '<hr>';
+	echo '</li>';
 } ?>
 </ol>
