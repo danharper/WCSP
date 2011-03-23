431abd02d85ffd8ac66d071e37340b741ed1cbbe
Dan Harper <intouch@danharper.me>
2011-03-23 14:17:47 +0000

    product images display on home page. ugly code though, could be extracted

--- functional/controllers/product.php
+++ functional/controllers/product.php
@@ -9,7 +9,12 @@ class Product extends Controller {
 	function index() {
 		$this->set_title("Latest Products");
 		$products = DB::fetch('SELECT * FROM `products`');
+		$images = array();
+		foreach ($products as $p) {
+			$images[$p->id] = $this->get_main_image($p->id);
+		}
 		$this->add_payload("products", $products);
+		$this->add_payload("images", $images);
 		$this->render();
 	}
 
@@ -18,6 +23,14 @@ class Product extends Controller {
 		$product = DB::get('SELECT * FROM `products` WHERE `id` = '. $id);
 		$this->add_payload("product", $product);
 		$this->render();
+	}
+
+	private function get_all_images($id) {
+		return DB::fetch('SELECT * FROM `productimages` WHERE `product_id` = '. $id);
+	}
+
+	private function get_main_image($id) {
+		return DB::get('SELECT * FROM `productimages` WHERE `product_id` = '. $id .' AND `main` = 1 LIMIT 1');
 	}
 
 }
