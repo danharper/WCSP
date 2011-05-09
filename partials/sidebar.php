88ea14ed211bae77ce1f20831b63864f78cbeeb8
Dan Harper <intouch@danharper.me>
2011-05-09 22:29:08 +0100

    search cleanup

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -12,9 +12,6 @@
 		} ?>
 		<p class="view"><a href="<?php echo $this->link_to('basket'); ?>">View Basket</a></p>
 	</section>
-	<!-- <form id="search" action="<?php echo $this->link_to_path('search'); ?>" method="get"> -->
-	<?php echo $this->link_to('product', 'search'); ?>
-	<!-- <form id="search" action="<?php echo $this->link_to('product', 'search'); ?>" method="get"> -->
 	<form id="search" action="<?php echo ROOT; ?>" method="get">
 		<input type="hidden" name="route" value="product">
 		<input type="hidden" name="action" value="search">
